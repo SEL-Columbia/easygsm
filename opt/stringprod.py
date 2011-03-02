f0185dee7c55aa0330754002202a586756fb4ecf
Rajesh Menon <pm2587@columbia.edu>
2011-03-02 16:12:32 -0500

    added stringprod

---
+++ opt/stringprod.py
@@ -0,0 +1,23 @@
+# opt/stringprod.py
+
+from zope.interface import implements
+
+from twisted.internet.defer import succeed
+from twisted.web.iweb import IBodyProducer
+
+class StringProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
